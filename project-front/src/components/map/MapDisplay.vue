b7a38108fbc2699b3393c482dc6894c731bb7ac4
Nelsiemon <florent.roux@meritis.fr>
2023-03-12 12:26:26 +0100

    Add a map store to handle map interactions

--- project-front/src/components/map/MapDisplay.vue
+++ project-front/src/components/map/MapDisplay.vue
@@ -1,53 +1,16 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-import {
-  MAP_CENTER,
-  DEFAULT_ZOOM,
-  MIN_ZOOM,
-  MAX_ZOOM,
-  CONTINENT_DIMENSION,
-  GW2_ICON,
-  GW2_TILES_URL,
-} from '@/config/map.config';
 import { markers } from '@/assets/data/markers';
-// import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
-import { LatLngBounds } from 'leaflet';
-import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useMapStore } from '@/stores/map';
+
+const mapStore = useMapStore();
 
 const createMap = () => {
-  const map = L.map('gw2map', {
-    minZoom: MIN_ZOOM,
-    maxZoom: MAX_ZOOM,
-    zoom: DEFAULT_ZOOM,
-    center: MAP_CENTER,
-    crs: L.CRS.Simple,
-  });
-
-  const mapbounds = new LatLngBounds(
-    map.unproject([0, 0], map.getMaxZoom()),
-    map.unproject(CONTINENT_DIMENSION, map.getMaxZoom()),
-  );
-  map.setMaxBounds(mapbounds);
-
-  // Add map tiles using the [[API:Tile service]]
-  L.tileLayer(GW2_TILES_URL, {
-    minZoom: MIN_ZOOM,
-    maxZoom: MAX_ZOOM,
-    noWrap: false,
-  }).addTo(map);
-
-  map.on('click', (e) => console.log(e));
-
+  mapStore.createMap('gw2map');
+  mapStore.map?.on('click', (e) => console.log(e));
   markers.forEach((marker) => {
-    const markerOptions = {
-      title: marker.name,
-      clickable: true,
-      draggable: false,
-      icon: GW2_ICON,
-    };
-    const mapMarker = L.marker(marker.coordinates, markerOptions);
-    mapMarker.addTo(map);
+    mapStore.addMarker(marker);
   });
 };
 
---
+++ project-front/src/stores/map.ts
@@ -0,0 +1,58 @@
+import { defineStore } from 'pinia';
+import {
+  MAP_CENTER,
+  DEFAULT_ZOOM,
+  MIN_ZOOM,
+  MAX_ZOOM,
+  CONTINENT_DIMENSION,
+  GW2_ICON,
+  GW2_TILES_URL,
+} from '@/config/map.config';
+import { LatLngBounds, marker, map, tileLayer, CRS, type Map, type PointExpression } from 'leaflet';
+import type { MarkerData } from '@/assets/data/markers';
+
+export const useMapStore = defineStore({
+  id: 'map',
+  state: () => ({
+    map: null as Map | null,
+  }),
+  actions: {
+    createMap(divId: string) {
+      this.map = map(divId, {
+        minZoom: MIN_ZOOM,
+        maxZoom: MAX_ZOOM,
+        zoom: DEFAULT_ZOOM,
+        center: MAP_CENTER,
+        crs: CRS.Simple,
+      });
+
+      const mapbounds = new LatLngBounds(this.unproject([0, 0]), this.unproject(CONTINENT_DIMENSION));
+      this.map.setMaxBounds(mapbounds);
+
+      tileLayer(GW2_TILES_URL, {
+        minZoom: MIN_ZOOM,
+        maxZoom: MAX_ZOOM,
+        noWrap: false,
+      }).addTo(this.map as Map);
+    },
+    unproject(point: PointExpression) {
+      if (!this.map) {
+        throw new Error('Error: map is not defined');
+      }
+      return this.map.unproject(point, this.map.getMaxZoom());
+    },
+    addMarker(markerData: MarkerData) {
+      const markerOptions = {
+        title: markerData.name,
+        clickable: true,
+        draggable: false,
+        icon: GW2_ICON,
+      };
+      const mapMarker = marker(markerData.coordinates, markerOptions);
+      mapMarker.addTo(this.map as Map);
+    },
+    focusMarker(markerData: MarkerData) {
+      console.log('Focusing marker');
+    },
+  },
+});
